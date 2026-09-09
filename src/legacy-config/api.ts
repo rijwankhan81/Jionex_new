6e6cd7edbd86af4d71aaaab162b10831f607ca21
Rijwan <rijwanjaydee@gmail.com>
2026-09-09 09:34:33 +0530

    fix

--- src/legacy-config/api.ts
+++
@@ -1,5 +0,0 @@
-const apiBaseURL = process.env.API_HOST;
-const authServicePath = `${apiBaseURL}/auth/api/v1`;
-const storeServicePath = `${apiBaseURL}/store/api/v1`;
-
-export { apiBaseURL, authServicePath, storeServicePath };
